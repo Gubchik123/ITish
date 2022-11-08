feb71ba210b841f8777da723c122c295d9ab6363
Nikita <nikita.gubarev31@gmail.com>
2022-11-08 13:17:55 +0200

    Add the specials config classes

--- app/app.py
+++ app/app.py
@@ -1,4 +1,7 @@
 from flask import Flask
 
+from . import config
+
 
 app = Flask(__name__)
+app.config.from_object(config.ForTesting)
---
+++ app/config.py
@@ -0,0 +1,27 @@
+import os
+from dotenv import load_dotenv
+
+
+load_dotenv()
+
+
+class _Config:
+    DEBUG = False
+    TESTING = False
+    CSRF_ENABLED = True
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URI")
+
+class ForProduction(_Config):
+    ENV = "production"
+    SECRET_KEY=os.getenv("SECRET_KEY")
+
+class ForDevelopment(_Config):
+    DEVELOPMENT = True
+    ENV = "development"
+    SECRET_KEY = "secret_for_development_environment"
+
+class ForTesting(_Config):
+    TESTING = True
+    SQLALCHEMY_TRACK_MODIFICATIONS = True
+    SECRET_KEY = "secret_for_test_environment"
