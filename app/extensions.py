4df3d3260749e4a8ca3323a16a57841dc6f4aba7
Nikita <nikita.gubarev31@gmail.com>
2022-11-09 22:21:37 +0200

    Use Flask-Login extension

--- app/extensions.py
+++ app/extensions.py
@@ -11,4 +11,14 @@ db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
 # admin = Admin(app)
-# login_manager = LoginManager(app)
+
+
+### Flask-Login ###
+from . import models
+
+login_manager = LoginManager(app)
+
+
+@login_manager.user_loader
+def _load_user(id):
+    return models.User.query.filter(models.User.id == id).first()
