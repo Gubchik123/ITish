7365fc07c5a2ba98fd71f78ba09c01a88f47b4f0
Nikita <nikita.gubarev31@gmail.com>
2022-11-10 18:05:32 +0200

    Add login functionality

--- app/auth/funcs.py
+++ app/auth/funcs.py
@@ -1,5 +1,6 @@
 import flask
-from werkzeug.security import generate_password_hash
+import flask_login as flog
+from werkzeug.security import generate_password_hash, check_password_hash
 
 from ..models import User
 from ..extensions import db
@@ -7,10 +8,10 @@ from .forms import RegistrationForm, LoginAdminForm, LoginForm
 
 
 def _get_striped_(string: str) -> str:
-    return string.strip()
+    return str(string).strip()
 
 
-def _check_user_with_data_from_(form: RegistrationForm):
+def _check_registration_data_from_(form: RegistrationForm):
     if User.query.filter(User.email == _get_striped_(form.email.data)).first():
         flask.flash("There is the user with such email!", category="danger")
         return False
@@ -27,7 +28,7 @@ def _add_user_with_data_from_(form: RegistrationForm):
         User(
             email=_get_striped_(form.email.data),
             username=_get_striped_(form.username.data),
-            password=generate_password_hash(_get_striped_(form.username.data)),
+            password=generate_password_hash(_get_striped_(form.password.data)),
         )
     )
     db.session.commit()
@@ -39,19 +40,50 @@ def _add_user_with_data_from_(form: RegistrationForm):
 def sign_up_user():
     form = RegistrationForm()
 
-    if form.validate_on_submit() and _check_user_with_data_from_(form):
+    if form.validate_on_submit() and _check_registration_data_from_(form):
         return _add_user_with_data_from_(form)
 
     return flask.render_template("auth/registration.html", form=form)
 
 
+def _check_login_data_from_(form: LoginForm):
+    user = User.query.filter(User.email == form.email.data).first()
+
+    if not user:
+        flask.flash("There is not the user with such email!", category="danger")
+        return False
+
+    if not check_password_hash(user.password, _get_striped_(form.password.data)):
+        flask.flash("Wrong password!", category="danger")
+        return False
+
+    return True
+
+
 def log_in_user():
     form = LoginForm()
-    return flask.render_template("auth/login.html")
+
+    if flog.current_user.is_authenticated:
+        return flask.redirect(flask.url_for("profile.get_main_page"))
+
+    if form.validate_on_submit() and _check_login_data_from_(form):
+        flog.login_user(User.query.filter(User.email == form.email.data).first())
+
+        flask.flash("You have successfully logged in!", category="success")
+        return flask.redirect(
+            flask.request.args.get("next") or flask.url_for("get_home_page")
+        )
+
+    return flask.render_template("auth/login.html", form=form)
 
 
 def log_out_user():
-    pass
+    flog.logout_user()
+
+    flask.flash("You have successfully logged out!", category="success")
+    return flask.redirect(
+        flask.request.args.get("next") or flask.url_for("get_home_page")
+    )
 
 
 def log_in_admin():
