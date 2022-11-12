e3bd71d7e72524f06aef15cec5bde92a9f932354
Nikita <nikita.gubarev31@gmail.com>
2022-11-12 20:02:38 +0200

    Add upload folder to config

--- app/config.py
+++ app/config.py
@@ -9,18 +9,22 @@ class _Config:
     DEBUG = False
     TESTING = False
     CSRF_ENABLED = True
+    UPLOAD_FOLDER = "static"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URI")
 
+
 class ForProduction(_Config):
     ENV = "production"
-    SECRET_KEY=os.getenv("SECRET_KEY")
+    SECRET_KEY = os.getenv("SECRET_KEY")
+
 
 class ForDevelopment(_Config):
     DEBUG = True
     DEVELOPMENT = True
     ENV = "development"
     SECRET_KEY = "secret_for_development_environment"
+
 
 class ForTesting(_Config):
     TESTING = True
