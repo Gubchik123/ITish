bb590528a4bd5a159db590e2bcd504a894f2ad2c
Nikita <nikita.gubarev31@gmail.com>
2022-11-12 12:32:01 +0200

    Make some changes with view urls

--- app/blog/blueprint.py
+++ app/blog/blueprint.py
@@ -8,13 +8,8 @@ blog = Blueprint("blog", __name__)
 
 
 @blog.route("/")
-def get_all_posts():
-    return funcs.get_all_posts()
-
-
-@blog.route("/tags")
-def get_all_tags():
-    return funcs.get_all_tags()
+def get_blog_page():
+    return funcs.get_blog_page()
 
 
 @blog.route("/create-post", methods=["GET", "POST"])
@@ -28,24 +23,24 @@ def get_all_posts_with_(tag: str):
     return funcs.get_all_posts_with_(tag)
 
 
-@blog.route("/<post_url>")
+@blog.route("/post/<post_url>")
 def get_post_by_(post_url: str):
     return funcs.get_post_by_(post_url)
 
 
-@blog.route("/<post_url>/like")
+@blog.route("/post/<post_url>/like")
 @flog.login_required
 def like_post_with_(post_url: str):
     return funcs.like_post_with_(post_url)
 
 
-@blog.route("/<post_url>/comment", methods=["POST"])
+@blog.route("/post/<post_url>/comment", methods=["POST"])
 @flog.login_required
 def comment_post_with_(post_url: str):
     return funcs.comment_post_with_(post_url)
 
 
-@blog.route("/<post_url>/edit", methods=["GET", "POST"])
+@blog.route("/post/<post_url>/edit", methods=["GET", "POST"])
 @flog.login_required
 def edit_post_with_(post_url: str):
     return funcs.edit_post_with_(post_url)
