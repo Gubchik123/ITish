d97fc9d18ee336af20a20298709bb813249bb837
Nikita <nikita.gubarev31@gmail.com>
2022-11-29 14:42:10 +0200

    Refactoring in static files

--- app/static/js/blog/_global_variables.js
+++ app/static/js/blog/_global_variables.js
@@ -6,8 +6,3 @@ export const hidden_post_title_field = document.getElementById("post_title");
 export const hidden_post_body_field = document.getElementById("post_body");
 
 export const post_tags_field = document.getElementById("post_tags");
-
-export const create_post_btn = document.getElementById("post_submit");
-create_post_btn.addEventListener("click", get_click_event_on_create_post_btn);
-
-export let apply_btn, cancel_btn;
