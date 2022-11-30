57b9aa1cd38234bebca1e00fcd7de021f107053a
Nikita <nikita.gubarev31@gmail.com>
2022-11-30 10:20:46 +0200

    Fix small error with import

--- app/auth/forms/general.py
+++ app/auth/forms/general.py
@@ -2,7 +2,7 @@ import wtforms
 from flask_wtf import FlaskForm
 import wtforms.validators as validator
 
-from ..services import User, _get_striped_
+from ..services import User
 
 required = validator.InputRequired("The area must be filled!")
 
@@ -10,3 +10,15 @@ required = validator.InputRequired("The area must be filled!")
 def _there_is_user_with_such_email(email: str) -> bool:
     """For checking on exist user in database"""
     return bool(User.query.filter(User.email == email).first())
+
+
+def _get_striped_(string: str) -> str:
+    """For getting striped string
+
+    Args:
+        string (str): some string for striping
+
+    Returns:
+        str: striped string
+    """
+    return str(string).strip()
