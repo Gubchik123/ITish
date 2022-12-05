02b1974a8801336719b5c79103018153ef5b6ec2
Nikita <nikita.gubarev31@gmail.com>
2022-12-05 15:29:58 +0200

    Delete the tests connected with the 'About' page

--- tests/functional/test_general_pages.py
+++ tests/functional/test_general_pages.py
@@ -46,28 +46,3 @@ def test_FAQs_page_post(test_client):
     assert response.status_code == 405
     assert b"Method Not Allowed" in response.data
     assert b"FAQs" not in response.data
-
-
-def test_about_page_get(test_client):
-    """
-    GIVEN a Flask test client
-    WHEN the 'About' page is requested (GET)
-    THEN check the response is valid
-    """
-    response = test_client.get("/about")
-
-    assert response.status_code == 200
-    assert b"About" in response.data
-
-
-def test_about_page_post(test_client):
-    """
-    GIVEN a Flask test client
-    WHEN the 'About' page is posted to (POST)
-    THEN check that a '405' status code is returned
-    """
-    response = test_client.post("/about")
-
-    assert response.status_code == 405
-    assert b"Method Not Allowed" in response.data
-    assert b"About" not in response.data
