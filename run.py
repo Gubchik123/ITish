f54482e13400123b55f13e7a489c02873bdf0f9e
Nikita <nikita.gubarev31@gmail.com>
2022-12-09 15:39:59 +0200

    Remove 'gevent' module for running WSGI-Server

--- run.py
+++ run.py
@@ -1,5 +1,3 @@
-from gevent.pywsgi import WSGIServer
-
 import app.views
 import app.extensions
 
@@ -7,5 +5,4 @@ from app import app
 
 
 if __name__ == "__main__":
-    http_server = WSGIServer(("0.0.0.0", 5000), app)
-    http_server.serve_forever()
+    app.run()
