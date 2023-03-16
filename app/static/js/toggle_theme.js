6a84a17a1a6c720f3e742bc28b9d5e16a94e4956
Nikita <nikita.gubarev31@gmail.com>
2023-03-16 09:22:54 +0200

    Replace js sessionStorage with localStorage in toggle_theme.js

--- app/static/js/toggle_theme.js
+++ app/static/js/toggle_theme.js
@@ -3,7 +3,7 @@ const toggleBtn = document.querySelector("#toggle-theme");
 toggleBtn.addEventListener("click", function () {
 	if (document.documentElement.hasAttribute("theme")) {
 		toggleBtn.children[0].name = "moon";
-		sessionStorage.setItem("dark-theme", "no");
+		localStorage.setItem("dark-theme", "no");
 		document.documentElement.removeAttribute("theme");
 	} else {
 		set_dark_mode();
@@ -12,9 +12,9 @@ toggleBtn.addEventListener("click", function () {
 
 function set_dark_mode() {
 	toggleBtn.children[0].name = "sunny";
-	sessionStorage.setItem("dark-theme", "yes");
+	localStorage.setItem("dark-theme", "yes");
 	document.documentElement.setAttribute("theme", "dark");
 }
 
-if (sessionStorage.getItem("dark-theme") == "yes") set_dark_mode();
+if (localStorage.getItem("dark-theme") == "yes") set_dark_mode();
 else toggleBtn.children[0].name = "moon";
