115cc5e892ba3646f0069340130fa32534f1e3a9
Nikita <nikita.gubarev31@gmail.com>
2024-01-10 20:01:51 +0200

    Add BASE_DIR variable into the config.py

--- app/config.py
+++ app/config.py
@@ -1,8 +1,11 @@
 import os
+from pathlib import Path
 from dotenv import load_dotenv
 
 
-load_dotenv()
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+load_dotenv(BASE_DIR / ".env")
 
 
 class _BaseConfig:
